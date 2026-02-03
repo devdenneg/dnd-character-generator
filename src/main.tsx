a22f7a587eebaafea56100f25dc56f69f5d69c26
Denis Negodyaev <negodyaev-dn@zvuk.com>
2026-02-03 14:42:24 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MantineProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
-import { mantineTheme } from './theme/mantine-theme';
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider theme={mantineTheme}>
-      <Notifications position="top-right" />
-      <App />
-    </MantineProvider>
+    <App />
   </StrictMode>,
 );
